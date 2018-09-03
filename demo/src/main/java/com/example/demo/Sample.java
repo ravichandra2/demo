848787ef6e9621da64f6a37b8d0d9f7aa80540e3
nravi <nravi@LAPTOP-J7OOK2LD>
2018-09-02 22:57:30 -0700

    sample demo project

---
+++ demo/src/main/java/com/example/demo/Sample.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sample {
+	
+	void dis() {
+		System.err.println("this is sample method");
+	}
+
+}
